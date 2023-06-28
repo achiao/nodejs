66c254f01d2b920baeeed8bddf63cdc7764dac83
darrenh@synology.com <darrenh@synology.com>
2023-06-28 16:05:02 +0800

    fix some tweaks

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post("/", function (req, res) {
   let data = req.body;
-  const payload = data.events.message.text;
+  const payload = data.events[0].message.text;
   https.get(
      `https://chat.synology.com/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=%22e3UnCYgHYMB33SHq4QRG3aNOkj37uI3BepeZTPgcgn1EBbuAVVpJVAMOn8aCp76j%22&payload={"text":"${payload}"}`
   );
