e58e0c45c7b417386ffa20c23116be0a60300bf7
darrenh <darrenh@synology.com>
2023-06-30 16:30:11 +0800

    test: print event

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.post("/", function (req, res) {
   let data = req.body;
   const event = data.events[0];
-  const payload = event.message.text;
-  const userId = event.source.userId;
-  const accessToken = getAccessToken();
+  // const payload = event.message.text;
+  // const userId = event.source.userId;
+  // const accessToken = getAccessToken();
+  const payload = JSON.stringify(event);
   https.get(
     `https://chat.synology.com/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=%22e3UnCYgHYMB33SHq4QRG3aNOkj37uI3BepeZTPgcgn1EBbuAVVpJVAMOn8aCp76j%22&payload={"text":"${payload}"}`
   );
