ac0cb6c1f31b872bcd78c5d3d3074c4cc8e451d1
darrenh <darrenh@synology.com>
2023-07-10 13:55:51 +0800

    fix: change secret to dev env

--- index.js
+++ index.js
@@ -11,15 +11,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.post("/", async function (req, res) {
   let data = req.body;
   const event = data.events[0];
-  // in order to trigger deployment again
   const payload = `@channel ${event.message.text}`;
   const userId = event.source.userId;
+
   https.get(
-    `https://chat.synology.com/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=%22e3UnCYgHYMB33SHq4QRG3aNOkj37uI3BepeZTPgcgn1EBbuAVVpJVAMOn8aCp76j%22&payload={"text": "${payload}"}`
+    //`https://chat.synology.com/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=%22e3UnCYgHYMB33SHq4QRG3aNOkj37uI3BepeZTPgcgn1EBbuAVVpJVAMOn8aCp76j%22&payload={"text": "${payload}"}`
+    `https://chat.synology.com/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=%22as5yjkDScDRrb0d62NbGHMnYhshkQxYviFNzK9xToJOl7kwMjmvGaLTrJM4WFlDr%22&payload={"text": "${payload}"}`
   );
   const client = new line.Client({
     channelAccessToken:
-      "EYkQFUuJXrGi8YNH9KDAswSZO/y6OKeMc3fmgxWCg1uQcr94gfKqrejbiIHEO/JT5rJ9higpwew4GnX0lMYWAsM/NgCA9PZfLUjnD50AVQEC/TDVGpfKoM6oVecD4hxieov3f7Rq/EVd/qY8jLkChAdB04t89/1O/w1cDnyilFU=",
+      //"EYkQFUuJXrGi8YNH9KDAswSZO/y6OKeMc3fmgxWCg1uQcr94gfKqrejbiIHEO/JT5rJ9higpwew4GnX0lMYWAsM/NgCA9PZfLUjnD50AVQEC/TDVGpfKoM6oVecD4hxieov3f7Rq/EVd/qY8jLkChAdB04t89/1O/w1cDnyilFU=",
+      "aIIBTYWH4hjbZvzPnr8Nr5wb6ztCwN7W5H5MbUU/fDGdrjhpY24Tfjhyx/TTGwlsK0XRBJMCxqHUD9yoeXlIyV+y6LIcqVg4JZ9Y+CZX/7hFADvLasSfN5o0StgtftnlalMYEJXFkRpGgdfEG87aMQdB04t89/1O/w1cDnyilFU=",
   });
 
   const message = {
