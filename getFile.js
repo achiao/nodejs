154a4ab8c9ab7352d49c47812eabb5614ff0158e
darrenh@synology.com <darrenh@synology.com>
2023-07-10 18:56:45 +0800

    feat: get file from line and upload it to imgur

---
+++ getFile.js
@@ -0,0 +1,31 @@
+const { ImgurClient } = require('imgur');
+
+async function getFileURL(messageId, client) {
+client.getMessageContent(messageId)
+  .then((stream) => {
+    var chunks = [];
+    stream.on('data', async (chunk) => {
+        chunks.push(chunk);
+    });
+    stream.on('error', (err) => {
+      // error handling
+    });
+    stream.on('end', async ()=> {
+        const base64Image = Buffer.from(Buffer.concat(chunks)).toString('base64');
+        const client = new ImgurClient({
+            clientId: '613a9d8ce8b43aa',
+            clientSecret: '4982e5f9d620eb6ba0e89bdb30e1a0001140f7d3',
+            refreshToken: '3b6930cc4dc2b6691e7bca0490248dbbd18acd4e',
+          });
+
+          const response = await client.upload({
+            image: base64Image,
+            type: 'base64',
+          });
+          console.log(response.data);
+        const fileURL = response.data.link;
+        resolve(fileURL)
+    });
+  });
+}
+module.exports = getFileURL
