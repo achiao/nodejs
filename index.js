ddb68bd2592e555798484059e83a3c8eb8817d73
darrenh <darrenh@synology.com>
2023-07-10 10:43:39 +0800

    feature: add @channel before any messages

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.post("/", async function (req, res) {
   let data = req.body;
   const event = data.events[0];
-  const payload = event.message.text;
+  const payload = `@channel ${event.message.text}`;
   const userId = event.source.userId;
   https.get(
     `https://chat.synology.com/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=%22e3UnCYgHYMB33SHq4QRG3aNOkj37uI3BepeZTPgcgn1EBbuAVVpJVAMOn8aCp76j%22&payload={"text": "${payload}"}`
