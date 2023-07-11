a0a70d2a53489a1b2605dfc61d618dd1d5e3eea7
darrenh <darrenh@synology.com>
2023-07-11 11:20:35 +0800

    fix: use the env variables

--- getFile.js
+++ getFile.js
@@ -15,9 +15,9 @@ module.exports.getFileURL = async function getFileURL(messageId, client) {
           "base64"
         );
         const client = new ImgurClient({
-          clientId: "613a9d8ce8b43aa",
-          clientSecret: "4982e5f9d620eb6ba0e89bdb30e1a0001140f7d3",
-          refreshToken: "3b6930cc4dc2b6691e7bca0490248dbbd18acd4e",
+          clientId: process.env.CLIENT_ID,
+          clientSecret: process.env.CLIENT_SECRET,
+          refreshToken: process.env.REFRESH_TOKEN,
         });
 
         const response = await client.upload({
--- index.js
+++ index.js
@@ -15,9 +15,8 @@ app.post("/", async function (req, res) {
   const message = data.events[0].message;
   const type = message.type;
   const client = new line.Client({
-    channelAccessToken:
-      //"EYkQFUuJXrGi8YNH9KDAswSZO/y6OKeMc3fmgxWCg1uQcr94gfKqrejbiIHEO/JT5rJ9higpwew4GnX0lMYWAsM/NgCA9PZfLUjnD50AVQEC/TDVGpfKoM6oVecD4hxieov3f7Rq/EVd/qY8jLkChAdB04t89/1O/w1cDnyilFU=",
-      "aIIBTYWH4hjbZvzPnr8Nr5wb6ztCwN7W5H5MbUU/fDGdrjhpY24Tfjhyx/TTGwlsK0XRBJMCxqHUD9yoeXlIyV+y6LIcqVg4JZ9Y+CZX/7hFADvLasSfN5o0StgtftnlalMYEJXFkRpGgdfEG87aMQdB04t89/1O/w1cDnyilFU=",
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    //"EYkQFUuJXrGi8YNH9KDAswSZO/y6OKeMc3fmgxWCg1uQcr94gfKqrejbiIHEO/JT5rJ9higpwew4GnX0lMYWAsM/NgCA9PZfLUjnD50AVQEC/TDVGpfKoM6oVecD4hxieov3f7Rq/EVd/qY8jLkChAdB04t89/1O/w1cDnyilFU=",
   });
 
   let payload = "";
@@ -33,7 +32,7 @@ app.post("/", async function (req, res) {
   }
   https.get(
     //`https://chat.synology.com/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=%22e3UnCYgHYMB33SHq4QRG3aNOkj37uI3BepeZTPgcgn1EBbuAVVpJVAMOn8aCp76j%22&payload={"text": "${payload}"}`
-    `https://chat.synology.com/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=%22as5yjkDScDRrb0d62NbGHMnYhshkQxYviFNzK9xToJOl7kwMjmvGaLTrJM4WFlDr%22&payload={"text": "${payload}", "file_url": "${fileURL}"}`
+    `https://chat.synology.com/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=%22${process.env.CHAT_TOKEN}%22&payload={"text": "${payload}", "file_url": "${fileURL}"}`
   );
   const userId = event.source.userId;
   const responseMessage = {
